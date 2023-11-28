8f6ae76bdc1d3644ac218ad3a51dc9dd1192eb19
TheCloud <100693885+ImTheCloud@users.noreply.github.com>
2023-11-28 19:27:55 +0100

    Add directory + start character

---
+++ src/main/java/be/helb/cpopadiuc/model/Character.java
@@ -0,0 +1,25 @@
+package be.helb.cpopadiuc.model;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String bounty;
+    private String rank;
+    private String job;
+    private String race;
+
+
+   // @ManyToOne
+   // @JoinColumn(name = "course_id") // Le nom de la colonne dans la table "Student" qui fait référence à "Course"
+  //  private Course course;
+}
